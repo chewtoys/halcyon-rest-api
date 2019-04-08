bf417f7e9f519fc5a576ac82b4cdcfce719d74f6
Chris Poulter <cpoulter@hotmail.co.uk>
2019-04-08 10:28:58 +0100

    code clean up

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,8 @@ import app from './app';
 
 const port = process.env.PORT || 3001;
 
-const server = app.listen(port, () => {
-    console.log(`Express server listening on port ${port}`);
-});
+const server = app.listen(port, () =>
+    console.log(`Express server listening on port ${port}`)
+);
 
 export default server;
