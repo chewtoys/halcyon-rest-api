749499ac59ae265d7c820f9d64ea93a871d971e0
Chris Poulter <cpoulter@hotmail.co.uk>
2019-04-19 10:54:56 +0100

    updated email regex

--- src/utils/string.ts
+++ src/utils/string.ts
@@ -27,7 +27,7 @@ export const capitalize = (value: string) =>
         .join(' ');
 
 export const isValidEmailAddress = (value: string) =>
-    /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(value);
+    /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(value);
 
 export const isValidISODate = (value: string) =>
     /^(-?(?:[1-9][0-9]*)?[0-9]{4})-(1[0-2]|0[1-9])-(3[01]|0[1-9]|[12][0-9])T(2[0-3]|[01][0-9]):([0-5][0-9]):([0-5][0-9])(.[0-9]+)?(Z)?$/.test(
